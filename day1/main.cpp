12eaae47931b95569a3c452416c2845b7b20b2ad
addja <daniel.oteroavalle@gmail.com>
2018-12-22 14:23:04 +0000

    Day one challenge 2 down

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
+#include<set>
+#include<vector>
 
 using namespace std;
 
 int
 main() {
-	int tmp;
 	int count = 0;
-	while (cin >> tmp) count += tmp;
-	cout << count  << endl;
+	int i;
+	set<int> mem = { 0 };
+	vector<int> input;
+	while ( cin >> i ) input.push_back( i );
+	i = 0;
+	while ( true ) {
+		count += input[i];
+		if ( mem.count( count ) ) break;
+		mem.insert( count );
+		i == input.size()-1 ?  i = 0 : i++;
+	}
+	cout << count << endl;
 }
